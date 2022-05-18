5a1223f64a31c02b246a273f261be8c991f5b23a
doums85 <momoseck8@gmail.com>
2022-05-18 03:13:40 +0200

    show property template in create NFT

--- client/pages/nft/create.jsx
+++ client/pages/nft/create.jsx
@@ -37,14 +37,13 @@ export default function Create({ addressConnected, NFTs }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [NFTPropertie, setNFTPropertie] = useState();
   const [open, setOpen] = useState(false);
-  const [addressCollection, setAddressCollection] = useState([]);
   const [userCollections, setUserCollections] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!loading) {
       getCollections(userCollections, setUserCollections, NFTs);
-      setNFTPropertie()
+      setNFTPropertie();
       setLoading(true);
     }
   }, [NFTs, loading, userCollections]);
@@ -58,6 +57,7 @@ export default function Create({ addressConnected, NFTs }) {
   };
 
   console.log(NFTPropertie);
+
   return (
     <>
       <Heading
@@ -144,6 +144,20 @@ export default function Create({ addressConnected, NFTs }) {
               <i className="bx bxs-add-to-queue"></i>
             </Button>
           </ContainerProperties>
+
+          {NFTPropertie && (
+            <GroupProperties>
+              {NFTPropertie.map((property, index) => (
+                <Property key={index}>
+                  <small>Property #{index} </small>
+                  <p>
+                    {property[0]} : {property[1]}
+                  </p>
+                  <i className="bx bxs-x-circle"></i>
+                </Property>
+              ))}
+            </GroupProperties>
+          )}
 
           <Button style={{ marginTop: '2rem' }}>Create</Button>
         </Form>
@@ -355,6 +369,46 @@ const Form = styled.form`
 
     i {
       font-size: 1.5rem;
+    }
+  }
+`;
+
+const GroupProperties = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Property = styled.div`
+  position: relative;
+  display: grid;
+  gap: 1rem;
+  padding: 1rem 1.5rem;
+  color: var(--body-color);
+  background: var(--dark-color);
+  border-radius: 0.5rem;
+  box-shadow: var(--shadow);
+
+  small {
+    color: var(--first-color);
+    font-weight: 600;
+  }
+
+  i {
+    position: absolute;
+    top: -0.4rem;
+    right: -0.4rem;
+    color: crimson;
+    font-size: 1.2rem;
+    opacity: 0;
+  }
+
+  &:hover{
+    transform: scale(1.1);
+transition: 0.3s;
+    i{
+      opacity: 1;
+      transition: 0.1s;
     }
   }
 `;
